16c56a935825c33776ef4841901992c25c4afd10
Thanasis Politis <thanasis.politis@pcg.io>
2024-10-09 00:28:58 +0300

    Fooling around

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,32 @@
-module "sandbox" {
+module "sandbox1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "thanasis.politis+sandbox1@hey.com"
+    AccountName               = "sandbox-aft-1"
+    ManagedOrganizationalUnit = "AFT"
+    SSOUserEmail              = "thanasis.politis@hey.com"
+    SSOUserFirstName          = "Thanasis"
+    SSOUserLastName           = "Politis"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+module "sandbox2" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
@@ -25,3 +53,4 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
