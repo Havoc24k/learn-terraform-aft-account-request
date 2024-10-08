9f3826667c5ce0411b657a29b7fd3715db3c11d4
Thanasis Politis <thanasis.politis@pcg.io>
2024-10-09 00:49:50 +0300

    Delete sandbox accounts

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,56 +1,56 @@
-module "sandbox1" {
-  source = "./modules/aft-account-request"
+# module "sandbox1" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "thanasis.politis+sandbox1@hey.com"
-    AccountName               = "sandbox-aft-1"
-    ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "thanasis.politis@hey.com"
-    SSOUserFirstName          = "Thanasis"
-    SSOUserLastName           = "Politis"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "thanasis.politis+sandbox1@hey.com"
+#     AccountName               = "sandbox-aft-1"
+#     ManagedOrganizationalUnit = "AFT"
+#     SSOUserEmail              = "thanasis.politis@hey.com"
+#     SSOUserFirstName          = "Thanasis"
+#     SSOUserLastName           = "Politis"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "sandbox"
-}
+#   account_customizations_name = "sandbox"
+# }
 
-module "sandbox2" {
-  source = "./modules/aft-account-request"
+# module "sandbox2" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "thanasis.politis+sandbox2@hey.com"
-    AccountName               = "sandbox-aft-2"
-    ManagedOrganizationalUnit = "AFT"
-    SSOUserEmail              = "thanasis.politis@hey.com"
-    SSOUserFirstName          = "Thanasis"
-    SSOUserLastName           = "Politis"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "thanasis.politis+sandbox2@hey.com"
+#     AccountName               = "sandbox-aft-2"
+#     ManagedOrganizationalUnit = "AFT"
+#     SSOUserEmail              = "thanasis.politis@hey.com"
+#     SSOUserFirstName          = "Thanasis"
+#     SSOUserLastName           = "Politis"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "sandbox"
-}
+#   account_customizations_name = "sandbox"
+# }
 
